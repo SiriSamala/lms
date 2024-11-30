edb251d86a253a9b3bfeeb79049e7d3517ad9269
SiriSamala <sirisamala09@gmail.com>
2024-11-30 11:54:27 +0530

    evn setup and side bar

---
+++ lsm-website/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { AppSidebar } from '@/components/app-sidebar'
+
+export default function Home() {
+  return (
+    <div>
+      <AppSidebar />
+    </div>
+  )
+}
+
+
